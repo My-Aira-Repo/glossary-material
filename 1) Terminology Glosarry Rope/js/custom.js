373d83951a82a0f9c5b2ed306511d280ca79219b
maSzilard <maszilard87@gamil.com>
2018-09-06 10:37:10 +0300

    new data upload

---
+++ 1) Terminology Glosarry Rope/js/custom.js
@@ -0,0 +1,32 @@
+console.log("ez megy"); 
+jQuery.noConflict();
+(function ($) {
+    $(function () {
+                        
+            var tablerow = "";
+            var imgUrl = "";
+            // var imgUrl = "";
+
+        Papa.parse("./assets/data.csv", {
+            download: !0,
+            header: !0,
+            skipEmptyLines: true,
+            delimiter: ";",
+            error: function (err, file, inputElem, reason) 
+                { console.log(err, file, inputElem, reason); },
+            complete: function (results) {
+                    console.log(results);
+                $.each(results.data, function (index, val) {
+                    tablerow += "<tr>",
+                        tablerow += "<td>" + val['title'] + "</td>",
+                        tablerow += "<td>" + val['text'] + "</td>",
+                        tablerow += "<td><img src='" + imgUrl + "img/" + val['img'] + "' alt='placeholder'</td>",
+                    tablerow += "</tr>";    
+                });
+
+                $("#buyrope-terminology").html(tablerow);
+                
+            }
+        });
+    });
+})(jQuery);
