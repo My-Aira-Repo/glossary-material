404d775d5f49f6e0cf894a0b8564358e11aab76d
maSzilard <maszilard87@gamil.com>
2018-09-06 10:36:17 +0300

    Change of data upload

--- 1) Terminology Glosarry Rope/custom.js
+++ 1) Terminology Glosarry Rope/custom.js
@@ -1,36 +1,4 @@
-var data = {
-  0: {
-    title: "Agate",
-    text:
-      "Small type (usually 5.5 point) used for sports statistics, stock tables, classified ads, etc.",
-    img: "img/icon-img-placeholder.svg"
-  },
-  1: {
-    title: "Air",
-    text: "White space used in a story design.",
-    img: "img/icon-img-placeholder.svg"
-  },
-  2: {
-    title: "All caps",
-    text:"Type using only capital letters.",
-    img: "img/icon-img-placeholder.svg"
-  },
-  3: {
-      title: "Amberlith",
-      text:"An orange plastic sheet, placed over a pasted-up page, that contains shapes that the printer needs to screen, overprint or print in another color.",
-    img: "img/icon-img-placeholder.svg"
-  },
-  4: {
-    title: "Anchor",
-    text:  "White space used in a story design.",
-    img: "img/icon-img-placeholder.svg"
-  },
-  5: {
-    title: "Bumping/butting heads",
-    text:  "Headlines from adjacent stories that collide with each other. Should be avoided when possible. Also called tombstoning.",
-    img: "img/icon-img-placeholder.svg"
-  }
-};
+
 
 var tableElement = document.getElementById("buyrope-terminology");
 var tablerow = "";
@@ -44,5 +12,5 @@ Object.keys(data).forEach(function (key) {
     tablerow += "</tr>";    
 });
 
-tableElement.innerHTML = tablerow;
+// tableElement.innerHTML = tablerow;
 
